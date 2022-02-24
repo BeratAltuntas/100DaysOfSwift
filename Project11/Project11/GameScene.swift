8210adab0c888f015636bb2d9864eaaf2697b773
Celalettin Berat Altuntaş <cberataltuntas@icloud.com>
2022-02-24 12:42:50 +0300

    arkaplan, toplar, ve fizik eklendi.

--- Project11/Project11/GameScene.swift
+++ Project11/Project11/GameScene.swift
@@ -10,6 +10,39 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         
-      
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        MakeBouncer(at: CGPoint(x: 0, y: 0))
+        MakeBouncer(at: CGPoint(x: 256, y: 0))
+        MakeBouncer(at: CGPoint(x: 512, y: 0))
+        MakeBouncer(at: CGPoint(x: 768, y: 0))
+        MakeBouncer(at: CGPoint(x: 1024, y: 0))
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {return}
+        let location = touch.location(in: self)
+        
+        let ball = SKSpriteNode(imageNamed: "ballRed")
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2)
+        ball.physicsBody?.restitution = 0.5 // sıçrama
+        ball.position = location
+        addChild(ball)
+    }
+    
+    func MakeBouncer(at position : CGPoint){
+        
+        let bouncer = SKSpriteNode(imageNamed: "bouncer")
+        bouncer.position = position
+        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width/2)
+        bouncer.physicsBody?.isDynamic = false
+        addChild(bouncer)
+    
+    }
+
 }
