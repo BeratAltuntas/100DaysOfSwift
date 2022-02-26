8a48333630dc3d7e1635261a1a96c4ecd328b765
Celalettin Berat Altuntaş <cberataltuntas@icloud.com>
2022-02-26 08:44:06 +0300

    Proje12a ve Proje12b oluşturuldu.

---
+++ Project12a/Project10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by BERAT ALTUNTAŞ on 17.02.2022.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var label: UILabel!
+    
+}
---
+++ Project12b/Project10/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by BERAT ALTUNTAŞ on 19.02.2022.
+//
+
+import UIKit
+
+class Person: NSObject {
+
+    var name:String
+    var image:String
+    
+    init(name:String, image:String){
+        self.name = name
+        self.image = image
+    }
+}
