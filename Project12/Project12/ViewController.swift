ccb99595666327293946d1480422ad32e3732c27
Celalettin Berat Altuntaş <cberataltuntas@icloud.com>
2022-02-26 08:44:06 +0300

    Project12, Project12a ve Project12b oluşturuldu.

---
+++ Project12/Project12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project12
+//
+//  Created by BERAT ALTUNTAŞ on 26.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
