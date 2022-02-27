f515227a87b67e56f58ca62f64d9a80d88bb9259
Celalettin Berat Altuntaş <cberataltuntas@icloud.com>
2022-02-27 16:11:42 +0300

    Day50Challenge Dosyası oluşturuldu.

---
+++ Day50Challenge/Day50Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Day50Challenge
+//
+//  Created by BERAT ALTUNTAŞ on 27.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
