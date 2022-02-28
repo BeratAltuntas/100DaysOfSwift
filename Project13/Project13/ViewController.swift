f7a1a2542fcbb770df435902b8cdadd25ab0b0c8
Celalettin Berat Altuntaş <cberataltuntas@icloud.com>
2022-02-28 20:07:17 +0300

    Proje13 oluşturuldu.

---
+++ Project13/Project13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by BERAT ALTUNTAŞ on 28.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
