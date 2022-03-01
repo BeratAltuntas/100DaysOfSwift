04f4362fbf48c8a3ce412255a6be1ce1700589dd
Celalettin Berat Altuntaş <cberataltuntas@icloud.com>
2022-03-01 13:40:27 +0300

    Project14 Created

---
+++ Project14/Project14/GameScene.swift
@@ -0,0 +1,26 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by BERAT ALTUNTAŞ on 1.03.2022.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+        // Get label node from scene and store it for use later
+       
+        }
+      
+    
+    
+   
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+}
