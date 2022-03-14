48979689c9975501facb440f0d0e44fa8c33a046
Berat Altuntaş <cberataltuntas>
2022-03-14 22:45:57 +0300

    Project20 is created.

---
+++ Project20/Project20/Controllers/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  Project20
+//
+//  Created by BERAT ALTUNTAŞ on 14.03.2022.
+//
+
+import SpriteKit
+
+
+class GameScene: SKScene {
+
+    
+    override func didMove(to view: SKView) {
+        
+        
+    }
+  
+}
