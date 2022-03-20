037f5a727024f3217d73e17074b026f8a66384e0
Berat Altuntaş <cberataltuntas>
2022-03-20 14:20:28 +0300

    Project23 is created.

---
+++ Project23/Project23/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  Project23
+//
+//  Created by BERAT ALTUNTAŞ on 20.03.2022.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    
+    override func didMove(to view: SKView) {
+     
+    }
+}
