9467b146486071dcc6a4497284e8879a81595e77
Berat Altuntaş <cberataltuntas>
2022-04-02 23:21:45 +0300

    Project 28 challenge is done

--- Project28/Project28/Controllers/ViewController.swift
+++ Project28/Project28/Controllers/ViewController.swift
@@ -1,18 +1,20 @@
-//
-//  ViewController.swift
-//  Project28
-//
-//  Created by BERAT ALTUNTAŞ on 2.04.2022.
-//
+    //
+    //  ViewController.swift
+    //  Project28
+    //
+    //  Created by BERAT ALTUNTAŞ on 2.04.2022.
+    //
 import LocalAuthentication
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var secret: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Nothing to see here"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(saveSecretMessage))
+        navigationItem.rightBarButtonItem?.isEnabled = false
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
@@ -20,7 +22,7 @@ class ViewController: UIViewController {
         
     }
     
-
+    
     @IBAction func authenticate_TUI(_ sender: Any) {
         
         let context = LAContext()
@@ -32,9 +34,10 @@ class ViewController: UIViewController {
             context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { [weak self] success, authenticationError in
                 DispatchQueue.main.async {
                     if success{
+                        self?.navigationItem.rightBarButtonItem?.isEnabled = true
                         self?.unlockSecretMessage()
                     }else{
-                        //error
+                            //error
                         let ac = UIAlertController(title: "Authentication Failed", message: "You could not be verified; please try again.", preferredStyle: .alert)
                         ac.addAction(UIAlertAction(title: "OK", style: .default))
                         self?.present(ac,animated: true)
@@ -42,8 +45,37 @@ class ViewController: UIViewController {
                 }
             }
         }else{
-            // no biometry
-            let ac = UIAlertController(title: "Biometry unavailable", message: "Your device is not configured for biometric authentication.", preferredStyle: .alert)
+            
+            
+                // no biometry
+            let ac = UIAlertController(title: "Write your password", message: nil, preferredStyle: .alert)
+            ac.addTextField()
+            ac.addAction(UIAlertAction(title: "OK", style: .default,handler: {[weak ac,weak self] _ in
+                guard let text = ac?.textFields?[0].text else{return}
+                self?.checkPassword(text)
+            }))
+            present(ac, animated: true)
+        }
+        
+    }
+    
+    func checkPassword(_ text: String){
+        
+        let password = KeychainWrapper.standard.string(forKey: "LoginPassword") ?? ""
+        if password == text{
+            unlockSecretMessage()
+        }else if password == "" {
+            
+            let ac = UIAlertController(title: "Create Password", message: "You need to create a password", preferredStyle: .alert)
+            ac.addTextField()
+            ac.addAction(UIAlertAction(title: "OK", style: .default,handler: { [weak ac] _ in
+                guard let createPasswordText = ac?.textFields?[0].text else{return}
+                KeychainWrapper.standard.set(createPasswordText, forKey: "LoginPassword")
+            }))
+            present(ac, animated: true)
+        }else{
+            
+            let ac = UIAlertController(title: "Password Wrong!!", message: "Your password is wrong try again!", preferredStyle: .alert)
             ac.addAction(UIAlertAction(title: "OK", style: .default))
             present(ac, animated: true)
         }
@@ -64,24 +96,26 @@ class ViewController: UIViewController {
         }
         
         secret.scrollIndicatorInsets = secret.contentInset
-
+        
         let selectedRange = secret.selectedRange
         secret.scrollRangeToVisible(selectedRange)
     }
     
     func unlockSecretMessage(){
         secret.isHidden = false
+        navigationItem.rightBarButtonItem?.isEnabled = true
         title = "Secret Stuff!"
         
         secret.text = KeychainWrapper.standard.string(forKey: "SecretMessage") ?? ""
     }
     
-   @objc func saveSecretMessage(){
+    @objc func saveSecretMessage(){
         guard secret.isHidden == false else{return}
         
         KeychainWrapper.standard.set(secret.text, forKey: "SecretMessage")
         secret.resignFirstResponder()
         secret.isHidden = true
+        navigationItem.rightBarButtonItem?.isEnabled = false
         title = "Nothing to see here"
     }
 }
